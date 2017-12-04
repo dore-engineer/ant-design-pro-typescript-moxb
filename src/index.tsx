f6e62ac86ec0ffcab02eb2a0246251e08c6e3b90
phupn <phupn@mail.it.dore.vn>
2017-12-04 16:16:15 +0700

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import 'babel-polyfill';
-import dva from 'dva';
-import 'moment/locale/zh-cn';
+import 'moment/locale/vi';
 import './g2';
-// import './rollbar';
-// import browserHistory from 'history/createBrowserHistory';
+import browserHistory from 'history/createBrowserHistory';
 import './index.less';
 import router from './router';
 
-// 1. Initialize
-const app = dva({
-  // history: browserHistory(),
-});
-
-// 2. Plugins
-// app.use({});
-
-// 3. Register global model
-// app.model(require('./models/global'));
-
-// 4. Router
-app.router(router);
-
-// 5. Start
-app.start('#root');
+ReactDOM.render(
+    router(browserHistory()),
+    document.getElementById('root')
+);
